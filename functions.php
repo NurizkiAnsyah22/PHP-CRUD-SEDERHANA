913ab95bd18b5eda6ada784d18baa14233c8fb03
Nurizkiansyah22 <nurizkiansyah02@gmail.com>
2023-09-10 01:05:45 +0700

    menambah fitur upload file

--- functions.php
+++ functions.php
@@ -21,7 +21,12 @@ function tambah($data)
     $nama = htmlspecialchars($data["nama"]);
     $email = htmlspecialchars($data["email"]);
     $jurusan = htmlspecialchars($data["jurusan"]);
-    $gambar = htmlspecialchars($data["gambar"]);
+//upload gambar
+    $gambar = upload() ;
+        if(!$gambar) {
+            return false;
+        }
+    
 
     $query = "INSERT INTO mahasiswa
 VALUES
@@ -39,6 +44,54 @@ function hapus($id)
 }
 
 
+function upload() {
+    
+    $fileUpload = $_FILES["gambar"]["name"];
+    $sizeFile = $_FILES["gambar"]["size"];
+    $error = $_FILES["gambar"]["error"];
+    $tmpSmtra = $_FILES["gambar"]["tmp_name"];
+
+    //cek apakah ada file gambar yang di upload
+
+    if( $error === 4) {
+        echo "<script> 
+        alert('Silahkan upload gambar terlebih dahulu!')
+        </script>";
+        return false; 
+    }
+
+    //cek extension gambar
+    $extGambarValid = ["jpg", "jpeg","png"];
+    $extensiGambar = explode(".", $fileUpload);
+    $extensiGambar = strtolower(end($extensiGambar));
+
+    if(!in_array($extensiGambar, $extGambarValid)) {
+        echo "<script> 
+        alert('File yang anda masukan bukan gambar!')
+        </script>";
+        return false; 
+    }
+
+    //cek ukuran gambar di bawah 1 mb
+    if($sizeFile > 1000000) {
+        echo "<script> 
+        alert('Silahkan upload gambar di bawah 1MB!')
+        </script>";
+        return false; 
+    }
+
+    //generate nama gambar baru
+    
+    $fileUploadBaru = uniqid();
+  $fileUploadBaru .= ".";
+  $fileUploadBaru .= $extensiGambar;
+    
+    move_uploaded_file($tmpSmtra,"img/" . $fileUploadBaru);
+return $fileUploadBaru;    
+}  
+
+    
+
 
 
 function ubah($data) {
@@ -48,7 +101,16 @@ function ubah($data) {
     $nama = htmlspecialchars($data["nama"]);
     $email = htmlspecialchars($data["email"]);
     $jurusan = htmlspecialchars($data["jurusan"]);
-    $gambar = htmlspecialchars($data["gambar"]);
+    $gambarLama = htmlspecialchars($data["gambarLama"]);
+
+ // Cek apakah tidak ada berkas yang diunggah
+ if ($_FILES['gambar']['error'] === 4 && $_FILES['gambar']['size'] > 1000000) {
+    $gambar = $gambarLama;
+} else {
+    $gambar = upload();
+}
+
+   
 
     $query = "UPDATE mahasiswa SET
                 nim = '$nim',
--- ubah.php
+++ ubah.php
@@ -12,7 +12,7 @@ $mhsUbah = query("SELECT * FROM mahasiswa WHERE id= $id")[0];
 //cek apakah tombol sudah di submit
 if (isset($_POST["submit"])) {
 
-    if ( ubah($_POST) > 0) 
+    if ( ubah($_POST) > 0 && $_FILES['gambar']['size'] <= 1000000) 
     {
         echo "
         <script> alert('Data berhasil di ubah!');
@@ -20,6 +20,7 @@ if (isset($_POST["submit"])) {
         </script>
         ";
     } else {
+        
         echo "
         <script> alert('Data gagal di ubah!');
         document.location.href='index.php';
@@ -44,9 +45,10 @@ if (isset($_POST["submit"])) {
 <body>
     <h1>Ubah data</h1>
 
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <ul>
             <input type="hidden" name="id" id="id" value="<?= $mhsUbah["id"]?>">
+            <input type="hidden" name="gambarLama" id="id" value="<?= $mhsUbah["gambar"]?>">
 
             <li> <label for=" nim">Nim : </label>
                 <input type="text" name="nim" id="nim" required value="<?= $mhsUbah["nim"]?>">
@@ -64,12 +66,13 @@ if (isset($_POST["submit"])) {
                 <input type="text" name="jurusan" id="jurusan" required value="<?= $mhsUbah["jurusan"]?>">
             </li>
             <br>
-            <li> <label for="gambar">Gambar : </label>
-                <input type="text" name="gambar" id="gambar" required value="<?= $mhsUbah["gambar"]?>">
+            <li> <label for="gambar">Gambar : </label> <br>
+                <img src="img/<?= $mhsUbah["gambar"]?>" width="50px"> <br>
+                <input type="file" name="gambar" id="gambar">
             </li>
             <br>
             <li>
-                <button type="submit" name="submit">Ubah</button>
+                <button type=" submit" name="submit">Ubah</button>
             </li>
 
         </ul>
